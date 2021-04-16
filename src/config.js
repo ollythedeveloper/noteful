b71212a1fed2dfe601bca1609760160fab00723c
Olivia Lewis <ollybaby12@gmail.com>
2021-04-15 23:36:28 -0400

    config to deployed server db

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export default {
-    // NOTE_URL:'http://localhost:9090/notes',
-    // FOLDER_URL:'http://localhost:9090/folders',
-    NOTE_URL:'http://localhost:8000/api/notes',
-    FOLDER_URL: 'http://localhost:8000/api/folders'
+    NOTE_URL:'https://murmuring-dawn-74135.herokuapp.com/api/notes',
+    FOLDER_URL: 'https://murmuring-dawn-74135.herokuapp.com/api/folders'
 }
